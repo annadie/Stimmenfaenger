99f01e39c2b7af7a88bc5b8ddce74a0fba29ba3e
annadie <annadiessner.design@gmail.com>
2025-08-28 15:56:41 +0200

    Add files via upload
    combination of speech to text and its attention scoring of keywords

--- simple_test.py
+++ simple_test.py
@@ -2,23 +2,38 @@ if __name__ == '__main__':
 
     import os
     import sys
+    from keybert import KeyBERT
     if os.name == "nt" and (3, 8) <= sys.version_info < (3, 99):
         from torchaudio._extension.utils import _init_dll_path
         _init_dll_path()
 
     from RealtimeSTT import AudioToTextRecorder
 
+    # Initialize the KeyBERT model
+    model = KeyBERT('sentence-transformers/LaBSE')
+
+
     recorder = AudioToTextRecorder(
-        spinner=False,
-        silero_sensitivity=0.01,
-        model="tiny",
-        language="de",
-        )
+            spinner=False,
+            silero_sensitivity=0.01,
+                model="tiny",
+            language="de",
+            )
 
     print("Say something...")
     
     try:
         while (True):
-            print(recorder.text())
+            text=recorder.text()
+            # Extract keywords
+            keywords = model.extract_keywords(text)
+
+            # Print the keywords
+            print(text)
+            print("Keywords:")
+            for keyword in keywords:
+                print(keyword)
+            print("----------------------------")
+
     except KeyboardInterrupt:
         print("Exiting application due to keyboard interrupt")
